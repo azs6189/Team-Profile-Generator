723f537a729d819ce5b44b764822b6d0e5e14066
Allie Soliz <solizale@gmail.com>
2021-10-15 10:49:15 -0400

    Employee.test updated

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,32 @@
+// using Employee constructor
+const Employee = require("../lib/Employee");
+
+// creates employee object
+test("creates an employee object", () => {
+	const employee = new Employee("Jen", 90, "jen.elisaw@gmail.com");
+
+	expect(employee.name).toEqual(expect.any(String));
+	expect(employee.id).toEqual(expect.any(Number));
+	expect(employee.email).toEqual(expect.any(String));
+});
+
+// gets id from getId()
+test("gets employee name", () => {
+	const employee = new Employee("Jen", 90, "jen.elisaw@gmail.com");
+
+	expect(employee.getName()).toEqual(expect.any(String));
+});
+
+// gets id from getId()
+test("gets employee ID", () => {
+	const employee = new Employee("Jen", 90, "jen.elisaw@gmail.com");
+
+	expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+// gets emails from getEmail()
+test("gets employee email", () => {
+	const employee = new Employee("Jen", 90, "jen.elisaw@gmail.com");
+
+	expect(employee.getRole()).toEqual("Employee");
+});
